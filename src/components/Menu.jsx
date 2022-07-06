803151972d688e6afe09f47d552f7a97757597ee
ebssgd <iamebssgd@yahoo.com>
2022-07-05 19:52:25 -0500

    Finished styling menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import playButton from "../img/videoPlayerButton.jpg"
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+import FeedIcon from '@mui/icons-material/Feed';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FlagIcon from '@mui/icons-material/Flag';
+import HelpIcon from '@mui/icons-material/Help';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Container = styled.div`
     flex:1;
     background-color: #202020;
-    height: 100vh;
+    height: 100%;
     color: white;
-    font-size: 14px;    
+    font-size: 14px;
+    position: sticky;
+    top: 0;    
 `;
 const Wrapper = styled.div`
     padding: 18px 26px;   
@@ -21,7 +39,32 @@ const Logo = styled.div`
 `;
 const Img = styled.img`
     height: 25px;
-`
+`;
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    cursor: pointer;
+    padding: 7.5px 0px;
+`;
+const Hr = styled.hr`
+    margin: 15px 0px;
+    border: 0.5px solid #373737;
+`;
+const Login = styled.div``;
+const Button = styled.button`
+    padding: 5px 15px;
+    background-color: transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    border-radius: 3px;
+    font-weight: 500;
+    margin-top: 10px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
 
 const Menu = () => {
   return (
@@ -31,6 +74,77 @@ const Menu = () => {
                 <Img src={playButton} />
                 MeTube
             </Logo>
+            <Item>
+                <HomeIcon />
+                Home
+            </Item>
+            <Item>
+                <ExploreIcon />
+                Explore
+            </Item>
+            <Item>
+                <SubscriptionsIcon />
+                Subscriptions
+            </Item>
+            <Hr/>
+            <Item>
+                <VideoLibraryIcon />
+                Library
+            </Item>
+            <Item>
+                <HistoryIcon />
+                History
+            </Item>
+            <Hr />
+            <Login>
+                Sign in to like videos, comment, and subscribe.
+                <Button>
+                    <AccountCircleIcon />
+                    SIGN IN
+                </Button>
+            </Login>
+            <Hr />
+            <Item>
+                <LibraryMusicIcon />
+                Music
+            </Item>
+            <Item>
+                <SportsFootballIcon />
+                Sports
+            </Item>
+            <Item>
+                <SportsEsportsIcon />
+                Gaming
+            </Item>
+            <Item>
+                <MovieCreationIcon />
+                Movies
+            </Item>
+            <Item>
+                <FeedIcon />
+                News
+            </Item>
+            <Item>
+                <LiveTvIcon />
+                Live
+            </Item>
+            <Hr />
+            <Item>
+                <SettingsIcon />
+                Settings
+            </Item>
+            <Item>
+                <FlagIcon />
+                Report
+            </Item>
+            <Item>
+                <HelpIcon />
+                Help
+            </Item>
+            <Item>
+                <LightModeIcon />
+                Light Mode
+            </Item>
         </Wrapper>
     </Container>
   )
